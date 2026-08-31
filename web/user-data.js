5fa78bac59ab0dad5dc40df3ed0dfe0a467edbfb
Jingde Clark <cashelclark.cajc@gmail.com>
2026-08-31 18:13:25 +0800

    Update user-data.js

--- web/user-data.js
+++ web/user-data.js
@@ -33,7 +33,7 @@
     };
 
     const CACHE_VERSION = "1";
-    const CACHE_TTL_MS = 5 * 60 * 1000;
+    const CACHE_TTL_MS = 30 * 60 * 1000;
     const CACHE_PREFIX = "cashewpapers:account-cache:";
 
     let activeUserId = null;
@@ -873,13 +873,6 @@
             writeStoredCache();
         }
     }
-
-    window.addEventListener(
-        "cashew-auth-change",
-        () => {
-            clearAccountCache();
-        }
-    );
 
     window.CashewUserData = {
         getUser,
